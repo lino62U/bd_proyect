3857b9e2a117368b7c186c812d5fc2dd562b059a
Avelino Lupo <103952759+lino62U@users.noreply.github.com>
2023-05-08 22:24:39 -0500

    Update disco.cpp

--- DISCO/disco.cpp
+++ DISCO/disco.cpp
@@ -2,6 +2,8 @@
 #include "vector"
 using namespace std;
 
+// typedef typename Sector*:: ptrSector;
+
 class Sector
 {
     private:
@@ -20,12 +22,13 @@ class Sector
         friend class Superficie;
         friend class Pista;      
 };
+typedef vector<Sector *> vSector;
 class Pista
 {
     private:
         int numero_sectores;
         int gap;
-        vector<Sector *> nSectores;
+        vSector nSectores;
     public:
         Pista(int nPista): numero_sectores(nPista) {}
     friend class Disco;
@@ -69,7 +72,34 @@ class Disco
             memoria_por_sector = nBytes;
             nPlatos.resize(numero_plato);
         }
-        void giro(){}
+        void giro()
+        {
+            int numero_superfice = 2;
+            for (size_t i = 0; i < numero_plato; i++)
+            {
+                if(nPlatos[i])
+                {
+                    for (size_t j = 0; j < numero_superfice; j++)
+                    {
+                        if(nPlatos[i]->nSuperficies[j])
+                        {
+                            for (size_t k = 0; k < numero_pistas_por_superficie; k++)
+                            {
+                                if(nPlatos[i]->nSuperficies[j]->nPistas[k])
+                                {
+                                    for (size_t l = 0; l < numero_sectores_por_pista; l++)
+                                    {
+                                        if(nPlatos[i]->nSuperficies[j]->nPistas[k]->nSectores[l])
+                                            cout<<nPlatos[i]->nSuperficies[j]->nPistas[k]->nSectores[l]->getData()<<endl;
+                                    }
+                                }
+                                
+                            }
+                        }        
+                    }
+                } 
+            }
+        }
         void cabezal_escritura(int pPlato, int pSuperficie, int pPista, int gap_Sector, const char *dato)
         {
             if(!nPlatos[pPlato]){ nPlatos[pPlato] = new Plato ();}
@@ -98,22 +128,34 @@ class Disco
                 if (nPlatos[pPlato]->nSuperficies[pSuperficie]->nPistas[pPista]->nSectores[gap_Sector + i])
                     bloque.push_back(nPlatos[pPlato]->nSuperficies[pSuperficie]->nPistas[pPista]->nSectores[gap_Sector + i]); 
             }
+            
             return bloque;
         }
         
-
 };
 
 int main()
 {
-    Disco disco(2, 100, 500, 5);
-    disco.cabezal_escritura(0, 0, 50, 200, "dos");
-    disco.cabezal_escritura(0, 0, 50, 201, "tres");
-    disco.cabezal_escritura(0, 0, 50, 202, "cuatro");
-    disco.cabezal_escritura(0, 0, 50, 203, "cinco");
+    Disco disco(2, 2, 3, 4);
+    disco.cabezal_escritura(0, 0, 0, 0, "hola");
+    disco.cabezal_escritura(0, 0, 0, 2, "dos");
 
-    vector <Sector *> b = disco.cabezal_lectura(0,0, 50,200,3);
-    for(auto i:b){
-        cout<<i->getData()<<endl;
-    }
+    disco.cabezal_escritura(0, 0, 1, 1, "tres");
+    
+    disco.cabezal_escritura(0, 1, 0, 0, "siete");
+    disco.cabezal_escritura(0, 1, 0, 1, "cinco");
+    disco.cabezal_escritura(0, 1, 0, 2, "seis");
+    
+    disco.cabezal_escritura(1, 0, 1, 2, "nueve");
+
+    disco.cabezal_escritura(1, 1, 1, 0, "diez");
+    
+    disco.giro();
+
+    // vector <Sector *> b = disco.cabezal_lectura(0,1, 0,1,2);
+    // for(auto i:b){
+    //     cout<<i->getData()<<endl;
+    // }
+
+    return 0;
 }
